a65b1988fef2453f5ac43306f0592cf899c218d6
isabel <isargp@gmail.com>
2022-04-13 09:50:00 +0100

    repos styled

--- src/components/RepoList/index.js
+++ src/components/RepoList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './index.css'
 
 export const RepoList = ({repos}) => {
 
@@ -9,11 +10,14 @@ export const RepoList = ({repos}) => {
          return (
             repos.map( repo => {
                counter++;
+               const date = repo.created_at.split('T')[0]
                return(
                   <div className="card" key={counter}>
-                     <h3>Name: {repo.name}</h3>
-                     <p>Language: {repo.language}</p>
-                     <a href={repo.html_url}><button>See repo</button></a>
+                     <h2 className="title">{repo.name}</h2>
+                     <p className="language">Language: {repo.language}</p>
+                     <p className="date">Created at: {date}</p>
+                     <p className="forks">Forks: {repo.forks}</p>
+                     <a href={repo.html_url}><button className="see-repo-bttn">See repo</button></a>
                   </div>)
             } ) )
       } catch(e) {
@@ -22,9 +26,10 @@ export const RepoList = ({repos}) => {
    }
 
    return (
-      <>
+      <div id="repos-wrapper">
+         <h1 id="repos-main-title">REPOSITORIES</h1>
          {renderRepos()}
-      </>
+      </div>
       
    )
 }
